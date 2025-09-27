76cc3d663a240212a34eec7ac298a6251ac56f26
CodeBandit01 <kramer5411xu@gmail.com>
2025-09-27 23:25:47 +0000

    api4

--- api/main.py
+++ api/main.py
@@ -1,10 +1,22 @@
-from fastapi import FastAPI
-from mangum import Mangum
+# from fastapi import FastAPI
+# from mangum import Mangum
 
-app = FastAPI()
+# app = FastAPI()
+
+from fastapi import FastAPI
+from fastapi.staticfiles import StaticFiles
+from fastapi.responses import FileResponse, HTMLResponse
+
+app = FastAPI(
+    title="Vercel + FastAPI",
+    description="Vercel + FastAPI",
+    version="1.0.0",
+)
+
+
+app.mount("/public", StaticFiles(directory="public"), name="public")
 
 @app.get("/api/hello")
 def read_root():
     return {"message": "Hello from FastAPI on Vercel!"}
 
-handler = Mangum(app)
